afd98637522f491712624cfe8ee01903bd24f010
Ballbot <ballbot@cashwalk.io>
2023-04-19 17:15:22 +0900

    initla commit

---
+++ src/components/Footers/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="relative bg-blueGray-200 pt-8 pb-6">
+        <div className="flex flex-wrap items-center md:justify-between justify-center">
+          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+            <div className="text-sm text-blueGray-500 font-semibold py-1">
+              © 2015-{new Date().getFullYear()} ballbot.dev, Designed by {" "}
+              <a
+                href="https://www.creative-tim.com?ref=nr-footer"
+                className="text-blueGray-500 hover:text-blueGray-800"
+              >
+                Creative Tim
+              </a>
+              .
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
---
+++ src/views/Index.js
@@ -0,0 +1,83 @@
+/*eslint-disable*/
+import React from "react";
+import { Link } from "react-router-dom";
+
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import Footer from "components/Footers/Footer.js";
+import "../assets/styles/customs/Main.css"
+
+export default function Index() {
+  return (
+    <>
+      <IndexNavbar fixed />
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <img
+          id="background-img"
+          style={{ zIndex: -1 }}
+          src={require("assets/img/wallpapers/minecraft-1.jpg").default}
+          alt="Wallpaper"
+        />
+        <div className="container mx-auto items-center flex flex-wrap" >
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0" style={{ maxWidth: 470 }}>
+              <h2 className="font-semibold"id="service-title">
+                내 PC 에서 만든 서버를 전 세계에 빠르게 호스팅
+              </h2>
+              <p className="mt-4 text-lg" style={{ color: "#E1E1E1" }}>
+                Waterflake는 여러분의 홈 서버가 모두에게 전달 될 수 있도록 돕습니다.
+                복잡한 네트워크 설정은 잊고 알찬 서버를 만들기 위해 더 많은 것을 투자하세요.
+              </p>
+              <ul id="service-title-ul">
+                <li>서버 주소에 포트 넘버 없음</li>
+                <li>선택 가능한 무료 도메인 주소</li>
+                <li>한국을 포함한 다양한 리전 선택 (예정)</li>
+              </ul>
+              <div className="mt-12">
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus?ref=nr-index"
+                  target="_blank"
+                  className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-orange-500 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                >
+                  무료로 시작하기
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container mx-auto px-4 pb-32 pt-48">
+          <div className="items-center flex flex-wrap">
+            <div className="w-full md:w-5/12 ml-auto px-12 md:px-4">
+              <div className="md:pr-12">
+                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
+                  <i className="fas fa-file-alt text-xl"></i>
+                </div>
+                <h3 className="text-3xl font-semibold">
+                  무료 베타 서비스
+                </h3>
+                <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                  Waterflake Project를 이용하면 복잡한 도메인, 방화벽 설정 없이 도메인 서버를 호스팅 할 수 있습니다. 현재 베타 서비스를 운영중이며, 추후 더 많은 기능과 서비스를 제공할 예정입니다.
+                </p>
+              </div>
+            </div>
+
+            <div className="w-full md:w-6/12 mr-auto px-4 pt-24 md:pt-0">
+              <img
+                alt="..."
+                className="max-w-full rounded-lg shadow-xl"
+                style={{
+                  transform:
+                    "scale(1) perspective(1040px) rotateY(-11deg) rotateX(2deg) rotate(2deg)",
+                }}
+                src={require("assets/img/documentation.png").default}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
